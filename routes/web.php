ef40da741e81c7041e5f549911f6ea55956851f4
ivandpoetra <49012541+ivandpoetra@users.noreply.github.com>
2024-09-10 22:14:50 +0700

    Update route supaya ke home

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/weather', [WeatherController::class, 'index']);
+Route::get('/', [WeatherController::class, 'index']);
